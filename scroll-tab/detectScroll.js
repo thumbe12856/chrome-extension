37cb0666c7194e2dad490b10c4b220baad1644cd
edward <thumbd12856@gmail.com>
2016-11-10 17:30:33 +0800

    Scroll tab with holding right click and scrolling mousewheel, but only can scroll one tab.

--- scroll-tab/detectScroll.js
+++ scroll-tab/detectScroll.js
@@ -1,13 +1,35 @@
-window.addEventListener('mousewheel', function(e){
-    wDelta = e.wheelDelta < 0 ? 'down' : 'up';
-    console.log(wDelta);
+var isRightClick = false;
 
-    console.log('Send message to scroll js.');
-	chrome.runtime.sendMessage({
-			action: 'scrolling',
-			direction: wDelta
-		}, function(response) {
-			console.log(response);
-		}
-	);
+window.addEventListener('mousewheel', function(e) {
+	if(isRightClick) {
+	    wDelta = e.wheelDelta < 0 ? 'down' : 'up';
+	    console.log(wDelta);
+
+	    console.log('Send message to scroll js.');
+		chrome.runtime.sendMessage({
+				action: 'scrolling',
+				direction: wDelta
+			}, function(response) {
+				console.log(response);
+				isRightClick = false;
+			}
+		);
+	}
+});
+
+//detect mouse down
+var timeoutId = 0;
+window.addEventListener("mousedown", function(e){
+	if (e.button === 2) {
+		timeoutId = setTimeout(rightClicking, 50);
+	}
+});
+function rightClicking() {
+	isRightClick = true;
+	console.log('right clicking.........');
+}
+
+window.addEventListener("mouseup", function(){
+	isRightClick = false;
+	clearTimeout(timeoutId);
 });
