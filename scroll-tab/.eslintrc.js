918dbfefd901e391a48bdaac52034ffd30942526
yanagiragi <qaz90473@gmail.com>
2019-07-14 03:40:02 +0800

    add eslint settings

---
+++ scroll-tab/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "semi": [2, "always"],
+        "quotes": [2, "double", "avoid-escape"],
+        "indent": ["error", "tab", {"SwitchCase": 1}],
+        "no-tabs": 0,
+        "linebreak-style": ["error", "unix"],
+    }
+};
