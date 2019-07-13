6eae3e175a576da78876d8e291015efaa1470996
yanagiragi <qaz90473@gmail.com>
2019-07-14 01:45:33 +0800

    update detectScroll for Firefox

--- scroll-tab/detectScroll.js
+++ scroll-tab/detectScroll.js
@@ -1,80 +1,68 @@
-// mouse wheel
-var mouseUpTimeOutId = 0;
-window.addEventListener("mousewheel", function(e) {
-	wDelta = 0;
-	if(e.wheelDelta < 0) {
-		wDelta = 1;
-	} else if (e.wheelDelta > 0) {
-		wDelta = -1;
-	}
 
-    if(wDelta) {
+// Debounce Limitation for lodash
+const DebounceLimit = 100;
+
+// ref: https://jsfiddle.net/6spj1059/
+const isFirefox = typeof InstallTrigger !== "undefined";
+const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+
+// Override window.chrome if is Firefox
+chrome = isFirefox ? window.browser : window.chrome;
+
+function ScrollHandler (event) {
+	function step (val) {
+		if (val > 0) return 1;
+		else if (val < 0) return -1;
+		else return 0;
+	}
+	const wDelta = step(event.wheelDelta || event.deltaY);
+	if (wDelta !== 0) {
 		chrome.runtime.sendMessage({
-				action: "scrolling",
-				direction: wDelta
-			}, function(response) {
-			}
-		);
-		//clearTimeout(mouseUpTimeOutId);
-		//mouseUpTimeOutId = setTimeout(mouseUp, 500);
+			action: "scrolling",
+			direction: wDelta
+		}, response => {});
+		// clearTimeout(mouseUpTimeOutId);
+		// mouseUpTimeOutId = setTimeout(mouseUp, 500);
 	}
-});
+}
 
-// mouse down
-var timeoutId = 0;
-window.addEventListener("mousedown", function(e) {
-	
-	// right click down
-	if (e.button === 2) {
-		// 100 ms to check that user is right clicking.
-		timeoutId = setTimeout(rightClicking, 100);
+function MouseDownHandler (event) {
+	if (event.button === 2) {
+		chrome.runtime.sendMessage({
+			action: "rightClickDown"
+		}, response => {});
 	}
+}
 
-	return false;
-});
+function MouseUpHandler (event) {
+	chrome.runtime.sendMessage({
+		action: "ClickUp"
+	}, response => removeDisableEventContextmenu());
+}
 
-// mouse up
-window.addEventListener("mouseup", function(e) {
-	clearTimeout(timeoutId);
-	clearTimeout(mouseUpTimeOutId);
-	mouseUp();
-});
+function disable (event) {
+	event.preventDefault();
+};
 
 // after user scrolls tab, and mouses up, disable the contextmenu.
 // when tab on focus, disable the contextmenu.
-window.addEventListener("focus", function() {
+function FocusHandler (event) {
 	window.addEventListener("contextmenu", disable, false);
 	setTimeout(removeDisableEventContextmenu, 1000);
-});
+}
 
 // when tab not on focus, enable the contextmenu.
-window.addEventListener("blur", function() {
-	window.removeEventListener("contextmenu", disable, false);
-});
-
-function rightClicking() {
-	chrome.runtime.sendMessage({
-			action: "rightClickDown"
-		}, function(response) {
-		}
-	);
-}
-
-function mouseUp() {
-	chrome.runtime.sendMessage({
-			action: "ClickUp"
-		}, function(response) {
-
-			// enable the contextmenu.
-			removeDisableEventContextmenu();
-		}
-	);
-}
-
-function removeDisableEventContextmenu() {
+function BlurHandler (event) {
 	window.removeEventListener("contextmenu", disable, false);
 }
 
-var disable = function(e) {
-	e.preventDefault();
+const scrollEventDetectionString = isFirefox ? "wheel" : "mousewheel";
+window.addEventListener(scrollEventDetectionString, _.debounce(ScrollHandler, DebounceLimit));
+window.addEventListener("mousedown", _.debounce(MouseDownHandler, DebounceLimit));
+window.addEventListener("mouseup", _.debounce(MouseUpHandler, DebounceLimit));
+window.addEventListener("focus", _.debounce(FocusHandler, DebounceLimit));
+window.addEventListener("blur", _.debounce(BlurHandler, DebounceLimit));
+
+function removeDisableEventContextmenu () {
+	window.removeEventListener("contextmenu", disable, false);
 }
