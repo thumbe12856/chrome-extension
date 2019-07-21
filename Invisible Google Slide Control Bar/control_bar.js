b69c0f2bb15e19c3ca5eae380144f25e873f8393
Your Name <you@example.com>
2019-07-21 22:30:37 +0800

    publish

--- Invisible Google Slide Control Bar/control_bar.js
+++ Invisible Google Slide Control Bar/control_bar.js
@@ -1,4 +1,6 @@
 var has_create = false;
+var direction = -1;
+var ori_position = "";
 
 (() => {
 
@@ -10,25 +12,31 @@ var has_create = false;
 	function GetDiv() {
 		var iframe = document.getElementsByClassName("punch-present-iframe");
 		var is_fail = false;
-		console.log("iframe", iframe);
+		var element = "";
 
 		if(!has_create) {
 			if(iframe.length > 0) {
 				var body = iframe[0].contentWindow.document.body;
-				console.log("body", body);
 
 				if(body) {
-					div = body.getElementsByClassName("punch-viewer-nav-rounded-container")[0];
-					has_create = true;
-					
-					var bar = control_element("bar");
-					div.appendChild(bar);
+					div = body.getElementsByClassName("punch-viewer-nav-rounded-container");
+					if(div.length > 0) {
+						div = div[0];
+						div.setAttribute("style", "left: 0; padding-right: 15px;");
+						//has_create = true;
 
-					var arrow = control_element("arrow");
-					div.appendChild(arrow);
+						var my_sperator = iframe[0].contentWindow.document.getElementById("my_sperator");
+						if(!my_sperator) {
+							var bar = control_element("bar");
+							div.appendChild(bar);
 
-					console.log("body: ", div);
-
+							var arrow = control_element("arrow");
+							div.appendChild(arrow);
+							arrow.addEventListener("click", function() {MoveHandler(div, arrow)}, true);
+						}
+					} else {
+						is_fail = true;
+					}
 				} else {
 					is_fail = true;
 				}
@@ -40,9 +48,11 @@ var has_create = false;
 		if(is_fail) {
 			setTimeout(GetDiv, 1000);
 		}
-		return;
+
+		return element;
 	}
 
+	// Build elements
 	function control_element(item) {
 		var element = "";
 		
@@ -56,25 +66,19 @@ var has_create = false;
 			node.setAttribute("style", "user-select: none;");
 			node.innerHTML += '&nbsp;';
 			element = node;
-			console.log("node", node);
 		} else if(item == "arrow") {
-			//var control_arrow = '<div class="goog-inline-block goog-flat-button" role="button" 
-			//aria-expanded="false" aria-haspopup="false" aria-disabled="false" style="user-select: none;" 
-			//tabindex="0">
-			//<div class="punch-viewer-icon-large punch-viewer-options goog-inline-block" 
-			//style="width: 14px;height: 50px;background: url(//i.imgur.com/EgMwR1u.png);"></div></div>'
 			var node1 = document.createElement("div");
 			node1.setAttribute("class", "goog-inline-block goog-flat-button");
 			node1.setAttribute("role", "button");
 			node1.setAttribute("aria-expanded", "false");
 			node1.setAttribute("aria-haspopup", "false");
 			node1.setAttribute("aria-disabled", "false");
-			node1.setAttribute("style", "user-select: none;");
+			node1.setAttribute("style", "transform: rotate(180deg);");
 			node1.setAttribute("tabindex", "0");
 			
 			var node2 = document.createElement("div");
 			node2.setAttribute("class", "punch-viewer-icon-large punch-viewer-options goog-inline-block");
-			node2.setAttribute("style", "width: 14px;height: 50px;background: url(//i.imgur.com/EgMwR1u.png); transform: rotate(180deg);");
+			node2.setAttribute("style", "width: 14px;height: 50px;background: url(//i.imgur.com/EgMwR1u.png);");
 
 			node1.appendChild(node2);
 			element = node1;
@@ -83,8 +87,36 @@ var has_create = false;
 		return element;
 	}
 
-	// Debounce Limitation for lodash
-	const DebounceLimit = 1000;
-	//document.getElementById("punch-start-presentation-left").addEventListener("mousedown", _.debounce(MouseDownHandler, DebounceLimit));
-	document.getElementById("punch-start-presentation-left").addEventListener("click", MouseDownHandler, true);
+	// Moving Animation
+	function MoveHandler(element, arrow) {
+		var pos = element.style.left;
+		pos = parseInt(pos.replace("px", "").replace("%", ""));
+		var id = setInterval(frame, 5);
+		var target_position = 0;
+		var target_angle = "transform: rotate(0deg);";;
+		var unit = 1 * direction;
+		if(direction < 0) {
+			target_position = -98;
+		} else {
+			target_position = 0;
+			target_angle = "transform: rotate(180deg);";
+		}
+
+		function frame() {
+			if (pos == target_position) {
+				clearInterval(id);
+				arrow.setAttribute("style", target_angle);
+				direction *= -1;
+			} else {
+				pos += unit;
+				element.style.left = pos + "%"; 
+			}
+		}
+	}
+
+	var start_button = document.getElementById("punch-start-presentation-left");
+	if(start_button) {
+		start_button.addEventListener("click", MouseDownHandler, true);
+	}
+	
 })();
